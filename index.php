261a2526556e2fba7258f8a8c24a4a048b1ef27a
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-13 01:06:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/Crock-Pot-6-Quart-Programmable-Stainless-SCCPVL610-S/dp/B004P2NG0K/ref=sr_1_27?s=kitchen-intl-ship&ie=UTF8&qid=1547321716&sr=1-27">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/Crock-Pot-6-Quart-Programmable-Stainless-SCCPVL610-S/dp/B004P2NG0K/ref=sr_1_27?s=kitchen-intl-ship&ie=UTF8&qid=1547321716&sr=1-27"
         </script>
         <title>Page Redirection</title>
     </head>
